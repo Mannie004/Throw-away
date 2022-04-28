97d800f7e1b21d7040dc3703a5c46012aaa7888d
Mannie004 <seun.adeyemi04@yahoo.com>
2022-04-28 11:09:43 -0400

    Added start cmd

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 // create express server
 const server = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 //make server listen
 server.listen(PORT, () => {
